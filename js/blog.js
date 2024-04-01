6db32803b51712b7dc64a8c2a1a4a0772a047606
Mirsad33 <mabedinoski3387@gmail.com>
2024-03-31 23:02:55 -0400

    changes to blogify

--- js/blog.js
+++ js/blog.js
@@ -1,30 +1,32 @@
 
-var blogPosts = [
-    {
-        title: "First Blog Post",
-        content: "This is my first blog post. Welcome to my blog!"
-    },
-    {
-        title: "Second Blog Post",
-        content: "This is my second blog post. Stay tuned for more content!"
-    },
-    {
-        title: "Third Blog Post",
-        content: "This is my first blog post. Welcome to my blog!"
-    },
-    {
-        title: "Fourth Blog Post",
-        content: "This is my first blog post. Welcome to my blog!"
-    },
+// var blogPosts = [
+//     {
+//         title: "First Blog Post",
+//         content: "This is my first blog post. Welcome to my blog!"
+//     },
+//     {
+//         title: "Second Blog Post",
+//         content: "This is my second blog post. Stay tuned for more content!"
+//     },
+//     {
+//         title: "Third Blog Post",
+//         content: "This is my first blog post. Welcome to my blog!"
+//     },
+//     {
+//         title: "Fourth Blog Post",
+//         content: "This is my first blog post. Welcome to my blog!"
+//     },
    
     
      
     
     
-];
+// ];
+var blogPosts = JSON.parse(localStorage.getItem("blogPost")) || []
 
+
+var blogContainer = document.getElementById("blog-container");
 function displayBlogPosts() {
-    var blogContainer = document.getElementById("blog-container");
 
     
     blogContainer.innerHTML = '';
@@ -35,13 +37,17 @@ function displayBlogPosts() {
         postDiv.classList.add("post");
 
         var titleElement = document.createElement("h2");
-        titleElement.textContent = post.title;
+        titleElement.textContent = post.blogTitle;
 
         var contentElement = document.createElement("p");
-        contentElement.textContent = post.content;
+        contentElement.textContent = post.blogContent;
+
+        var usernameElement = document.createElement("p") ;
+        usernameElement.textContent = post.username
 
         postDiv.appendChild(titleElement);
         postDiv.appendChild(contentElement);
+        postDiv.appendChild(usernameElement);
 
         blogContainer.appendChild(postDiv);
     });
